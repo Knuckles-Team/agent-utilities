1884cd3173a56570bcc1e5b7ba48430e8f421483
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-25 21:51:54 -0600

    Bump version: 0.2.1 → 0.2.2

--- agent_utilities/__init__.py
+++ agent_utilities/__init__.py
@@ -25,7 +25,7 @@ from .base_utilities import (
 from .embedding_utilities import create_embedding_model
 from .models import PeriodicTask
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = [
     "create_agent",
--- agent_utilities/api_utilities.py
+++ agent_utilities/api_utilities.py
@@ -3,7 +3,7 @@
 
 import functools
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 
 class AuthError(Exception):
